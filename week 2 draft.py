395dff9d5686b80f011633fbd62b1f77bb2260e4
KerollosEhab <21-101137@students.eui.edu.eg>
2024-02-21 12:56:19 +0200

    m

---
+++ week 2 draft.py
@@ -0,0 +1,35 @@
+#x=5
+#print(x)
+#for i in range(5):
+#    print(i)
+#a="xyz"
+#b="abc"
+#c=b+a
+#print(c)
+
+#x=1; y=0; z=not(x or y); print(z) 
+
+#x=2
+#while x%2==0:
+#    print(x)
+#    x=x/3
+
+#a="helloo"
+#if a=="hi":
+#    print("hi")
+#elif a=="hello":
+#    print("hey")
+#else: print("heyy")    
+
+#def function1(name):
+#    print("hello " + name + "!")
+#function1("kees")
+
+#def fn1(x):
+#    return x+1
+#print(fn1(5))
+
+#def fn1(x,y,z):
+#    return x+1, 2*y, z/3
+#(a,b,c)=fn1(2,4,6)
+#print(a,b,c)
